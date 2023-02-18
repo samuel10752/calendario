86a1c5af909069c09acc35bcbcf64897eea4fadd
Samuel Barbosa <102193242+samuel10752@users.noreply.github.com>
2023-02-17 21:13:03 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include("config/config.php"); ?>
+<?php include(DIRREQ."lib/html/header.php"); ?>
+
+    <a href="<?php echo DIRPAGE.'views/user'; ?>">Calendario do Usuario</a><br>
+    <a href="<?php echo DIRPAGE.'views/manager'; ?>">Calendario do Gerente</a>
+
+<?php include(DIRREQ."lib/html/footer.php"); ?>
+
